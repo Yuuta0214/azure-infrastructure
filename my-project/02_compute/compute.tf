c631ab2e5ae5764a3a08a4200b8f65e740a09818
Yuuta0214 <yuuta.ito0214@gmail.com>
2026-05-17 14:36:43 +0900

    change: to 02_compute

--- my-project/02_compute/compute.tf
+++ my-project/02_compute/compute.tf
@@ -76,3 +76,65 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   tags = local.common_tags
 }
+
+# ==========================================
+# 10. ネットワークインターフェース（NIC）の作成
+# ==========================================
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-${local.resource_prefix}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = local.target_subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = local.common_tags
+}
+
+# ==========================================
+# 11. NICとALBバックエンドプールの紐付け
+# ==========================================
+resource "azurerm_network_interface_backend_address_pool_association" "nic_assoc" {
+  network_interface_id    = azurerm_network_interface.nic.id
+  ip_configuration_name   = "internal"
+  backend_address_pool_id = local.target_be_pool_id
+}
+
+# ==========================================
+# 12. Linux 仮想マシン（VM）の作成
+# ==========================================
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-${local.resource_prefix}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+
+  disable_password_authentication = false
+  admin_password                  = var.admin_password
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    name                 = "osdisk-vm-${local.resource_prefix}"
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "debian"
+    offer     = "debian-12"
+    sku       = "12-gen2"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(templatefile("${path.module}/scripts/bootstrap.sh", {
+    hostname       = "vm-${local.resource_prefix}"
+    admin_username = var.admin_username
+  }))
+
+  tags = local.common_tags
+}
